4ce3e30200169a3e6526853ea0fbc004d542de6e
luiz <louisngatia47@gmail.com>
2026-06-08 01:42:51 +0300

    fixing errors

--- strath-eats/backend/api/urls.py
+++ strath-eats/backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StallViewSet, MenuItemViewSet, OrderViewSet, AdminStatsViewSet
-from views import trigger_stk_push,mpesa_callback
+from .views import trigger_stk_push, mpesa_callback
 
 router = DefaultRouter()
 router.register(r'stalls', StallViewSet)
